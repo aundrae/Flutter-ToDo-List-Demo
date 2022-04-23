6d873a082a583263ba79659bee064cb1b5f2982d
Justin Ramjattan <justinramjattan@outlook.com>
2022-04-22 20:40:48 -0400

    removed scrollview from add task

--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -16,52 +16,49 @@ class _AddTaskState extends State<AddTask> {
       appBar: AppBar(
         title: const Text("Add Task"),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              TextFormField(
-                onChanged: (text){
-                  setState(() {
-                    title = text;
-                  });
-                },
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              onChanged: (text){
+                setState(() {
+                  title = text;
+                });
+              },
 
-                decoration: const InputDecoration(
-                  labelText: 'Enter Task Title',
-                  border: OutlineInputBorder(),
-                ),
+              decoration: const InputDecoration(
+                labelText: 'Enter Task Title',
+                border: OutlineInputBorder(),
               ),
-              const SizedBox(height: 10),
-              TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: 10,
-                decoration: const InputDecoration(
-                  labelText: "Enter Description",
-                  border: OutlineInputBorder(),
-                ),
-                onChanged: (text){
-                  setState(() {
-                    description = text;
-                  });
-                },
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: 10,
+              decoration: const InputDecoration(
+                labelText: "Enter Description",
+                border: OutlineInputBorder(),
               ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(40), // fromHeight use double.infinity as width and 40 is the height
-                ),
-                onPressed: () {
-                  // Close the screen and return "Nope." as the result.
-                  Navigator.pop(context, jsonEncode({"title": title, "description": description}));
-                },
-                child: const Text('Submit'),
-              )
-            ],
-          ),
+              onChanged: (text){
+                setState(() {
+                  description = text;
+                });
+              },
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(40), // fromHeight use double.infinity as width and 40 is the height
+              ),
+              onPressed: () {
+                // Close the screen and return "Nope." as the result.
+                Navigator.pop(context, jsonEncode({"title": title, "description": description}));
+              },
+              child: const Text('Submit'),
+            )
+          ],
         ),
       ),
     );
